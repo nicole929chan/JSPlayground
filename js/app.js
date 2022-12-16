c1815ed48b1f615018de1a107187397257cb9cc0
Nicole <nicole929chan@gmail.com>
2022-12-16 17:14:17 +0800

    promise, async and await

--- js/app.js
+++ js/app.js
@@ -1,17 +1,53 @@
 console.log('enter app.js');
 
-const p1 = new Promise(function(resolve, reject) => {
-    setTimeout(() => {
-        resolve('p1 resolve message')
-    }, 2000)
-})
+// 近似callback hell阿
+// fetch('http://localhost:8001/popular?date=2022-12-16&rank=1') // 暢銷書籍排行榜
+//   .then((value) => value.json())
+//   .then(function (value) {
+//     let bookId = value[0].book_id;
+//     fetch(`http://localhost:8001/books?id=${bookId}`) // 暢銷書籍
+//       .then((value) => value.json())
+//       .then((value) => {
+//         let authorId = value[0].author_id;
+//         fetch(`http://localhost:8001/authors?id=${authorId}`) // 作者
+//           .then((value) => value.json())
+//           .then((value) => console.log(value));
+//       });
+//   })
+//   .catch((reason) => {
+//     console.log(reason);
+//   });
 
-p1.then(function(value) {
-    console.log('p1 then success')
-    console.log(value)
-}, function(reason) {
-    console.log('p1 then fail')
-    console.log(reason);
-})
+// async function findMostPopular() {
+//   try {
+//     // await fetch(), fetch返回Promise實例
+//     let res = await fetch(
+//       'http://localhost:8001/popular?date=2022-12-16&rank=1'
+//     )
+//       .then((value) => value.json())
+//       .then((value) => value);
+
+//     if (res.length === 0) {
+//       throw new Error('本週暢銷書籍尚未出爐');
+//     }
+//     let bookId = res[0].book_id;
+//     let res2 = await fetch(`http://localhost:8001/books?id=${bookId}`).then(
+//       (value) => value.json()
+//     );
+
+//     let authorId = res2[0].author_id;
+//     let res3 = await fetch(`http://localhost:8001/authors?id=${authorId}`).then(
+//       (value) => value.json()
+//     );
+//     console.log(res3); // 找到作者
+
+// 		// JS引擎預設慧返回undefined
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// findMostPopular();
+// // const p = findMostPopular()
 
 console.log('end app.js');
